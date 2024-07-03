3aa998100cd3c641ce10150c8f46009e77748de1
pradeeprajkk <pradeep.raj@accionlabs.com>
2024-07-03 11:41:52 +0530

    changes in vercel yaml

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-    res.send("Nodejs is awesome!!!!");
+    res.send("Nodejs is awesome!");
 });
 
 app.listen(4001, () => console.log('server is running on port',4001));
